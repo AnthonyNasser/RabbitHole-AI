732112dfafc61e256d91637208856a00f418e0be
Anthony Nasser <57681170+AnthonyNasser@users.noreply.github.com>
2023-10-25 17:50:39 -0700

    Initial Commit

---
+++ RabbitHole/RabbitHoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitHoleApp.swift
+//  RabbitHole
+//
+//  Created by Anthony Nasser on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RabbitHoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
